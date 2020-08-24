3a304810a9c8745a8b9b9572f5ad8016ba904542
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-08-24 12:53:52 +0200

    employee controller

---
+++ src/main/java/org/acardenosa/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package org.acardenosa.repository;
+
+import org.acardenosa.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
