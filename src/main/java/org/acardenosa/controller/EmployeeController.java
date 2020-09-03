f072c01491f2a6173cb0c567227cecf65f140cc4
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-09-03 18:40:14 +0200

    setting up exceptions and controller

--- src/main/java/org/acardenosa/controller/EmployeeController.java
+++ src/main/java/org/acardenosa/controller/EmployeeController.java
@@ -2,11 +2,14 @@ package org.acardenosa.controller;
 
 import java.util.List;
 
+import org.acardenosa.exception.ResourceNotFoundException;
 import org.acardenosa.model.Employee;
 import org.acardenosa.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +26,13 @@ public class EmployeeController {
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
 	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value="id") Long employeeId)
+			throws ResourceNotFoundException{
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id: "+employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+	
 }
