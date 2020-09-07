adb43b9c27f97e197666294c981a8168fa36fb61
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-09-07 15:55:09 +0200

    .

--- src/main/java/org/acardenosa/controller/EmployeeController.java
+++ src/main/java/org/acardenosa/controller/EmployeeController.java
@@ -7,9 +7,12 @@ import org.acardenosa.model.Employee;
 import org.acardenosa.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +38,8 @@ public class EmployeeController {
 		return ResponseEntity.ok().body(employee);
 	}
 	
+	@PostMapping("/employees/{id}")
+	public Employee createEmployee (@Validated @RequestBody Employee employee){
+		return employeeRepository.save(employee);
+	}
 }
